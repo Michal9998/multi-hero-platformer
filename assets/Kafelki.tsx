bfe74191770479b0201f260102c200304d81b73a
Michal9998 <mzawadzki328@gmail.com>
2026-06-03 15:58:13 +0200

    Add files via upload

---
+++ assets/Kafelki.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Kafelki" tilewidth="32" tileheight="32" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="dirt.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="tile.png" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="Fragile.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="spikes.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="lava.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="chest.png" width="32" height="32"/>
+ </tile>
+</tileset>
